990ecf488ba401837fa8667f1a2271f5ed9d911f
KarimRC <63407253+karim-rkha@users.noreply.github.com>
2024-11-06 17:38:57 +0100

    Update verify.js

--- api/verify.js
+++ api/verify.js
@@ -1,7 +1,9 @@
 export default function handler(req, res) {
-    // Set the required verification header directly in the response
+    // Set headers exactly as expected by Vercel
+    res.setHeader('Content-Length', '0');
     res.setHeader('x-vercel-verify', '84e391d43aa038d3a5d205d692a8798e95381721');
-    // Send a 200 OK status
+    
+    // Respond with a 200 status code
     res.status(200).end();
   }
   
